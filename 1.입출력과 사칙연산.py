697c4c8c85c65ba6351e80b8e20c0c40ec059957
Yoonbeom So <ybum1224@naver.com>
2021-12-23 17:56:31 +0900

    first commit

---
+++ 1.입출력과 사칙연산.py
@@ -0,0 +1,40 @@
+print("HelloWorld")
+
+print("강한친구 대한육군\n강한친구 대한육군")
+
+print("\\    /\\\n )  ( \')\n(  /  )\n \\(__)|")
+
+print("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|")
+
+A, B = map(int,input().split())
+print(A+B)
+
+A, B = map(int,input().split())
+print(A-B)
+
+A, B = map(int,input().split())
+print(A*B)
+
+A, B = map(float,input().split())
+print(A/B)
+
+A,B = map(int,input().split())
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
+
+A, B, C = map(int,input().split())
+print((A+B)%C)
+print(((A%C)+(B%C))%C)
+print((A*B)%C)
+print(((A%C)*(B%C))%C)
+
+A=int(input())
+B=int(input())
+
+print(A*(B%10))
+print(A*(B//10%10))
+print(A*(B//100))
+print(A*B)
