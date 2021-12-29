03916326600684bf8e763d6210b99f224ebafb84
Yoonbeom So <ybum1224@naver.com>
2021-12-29 22:54:45 +0900

    20211229 commit

--- 0.codingTest.py
+++ 0.codingTest.py
@@ -246,19 +246,19 @@
 #         mina = i
 # print(mina, maxa)
 
-## 2562번 최대값
+# # 2562번 최대값
 # su=[]
 # for i in range(0,9):
 #     su.append(int(input()))
 # maxa=su[0]
 # for i in su:
-#     if i >maxa:
+#     if i > maxa:
 #         maxa = i
 # #maxa=max(su)
 # print(maxa)
 # print(su.index(maxa)+1)
 
-##2577번 숫자의 개수
+# #2577번 숫자의 개수
 # A = int(input())
 # B = int(input())
 # C = int(input())
@@ -266,13 +266,42 @@
 # for i in range(10):
 #     print(result.count(str(i)))
 
-##3052번 나머지
-li=[]
-for i in range(10):
-    li.append((int(input()))%42)
-sli = set(li)
-print(len(sli))
+# #3052번 나머지
+# li=[]
+# for i in range(10):
+#     li.append((int(input()))%42)
+# sli = set(li)
+# print(len(sli))
 
-print("mac upload")
-print("20211226.")
+# #1546번 평균
+# ea = int(input())
+# li = list(map(int,input().split()))
+# li2 =[]
+# maxa=li[0]
+# for i in li:
+#     if i > maxa:
+#         maxa=i      
+# for i in li:
+#     li2.append(((i/maxa)*100))
+# print(int(sum(li2))/ea)  
+
+# #8958번 OX퀴즈
+ea = int(input())
+
+
+for i in range(ea):
+    data = input()
+    score = 0
+    point = 1
+    for i in data:
+        if i == "O":
+            score+=point
+            point+=1
+        else:
+            point = 1
+    print(score)
+
+
+# print("Windows Upload")
+# print("20211229")
 
