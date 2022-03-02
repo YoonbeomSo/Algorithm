8eef2807df343a512e5c2d1da7decd3f899017e9
YoonbeomSo <ybum1224@naver.com>
2022-03-02 22:47:15 +0900

    20220302

--- 0.codingTest.py
+++ 0.codingTest.py
@@ -531,11 +531,29 @@
 
 
 #10757번 큰 수 A+B
-a, b = map(int,input().split())
-print(a+b)
+# a, b = map(int,input().split())
+# print(a+b)
+
+
+#1978번 소수 찾기
+n = int(input())
+allsu = map(int,input().split())
+sosu=0
+
+for i in allsu:
+    check=0
+    if i >1 :
+        for j in range(2,i):
+            if i%j == 0:
+                check=1
+        if check==0: 
+            sosu +=1
+print(sosu)    
 
 
 
 
-# print("Windows Upload")
-# print("20220212")
+
+
+# print("20220302")
+
