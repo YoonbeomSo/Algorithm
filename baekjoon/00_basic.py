46cb86f0ee5785e3654560ab5141a9d24d7fcbca
soyoonbeom <syb1224@hecto.co.kr>
2022-12-12 13:02:21 +0900

    :zap: 리스트 컴프리핸션 문법 복습

--- baekjoon/00_basic.py
+++ baekjoon/00_basic.py
@@ -38,3 +38,24 @@
 # print(A*(B//10%10))
 # print(A*(B//100))
 # print(A*B)
+
+
+#리스트 컴프리핸션 -> 2차원 리스트 초기화시 반드시 리스트 컴프리헨션 사용
+array = [i for i in range(20) if i % 2 == 1]
+print(array)
+
+
+# N X M 크기의 2차원 리스트 초기화
+print("옳은 방법")
+n=3
+m=4
+array = [[0] * m for _ in range(n)]
+print(array)
+array[0][1] = 5
+print(array)
+# N X M 크기의 2차원 리스트 초기화 잘못된 방법
+print("잘못된 방법")
+array = [[0] * m] * n
+print(array)
+array[0][1] = 5
+print(array)
