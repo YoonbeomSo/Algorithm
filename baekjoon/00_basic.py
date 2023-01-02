b0ee55c0fd25275a1cac02860caf1a782579b78d
YoonbeomSo <ybum1224@naver.com>
2023-01-02 21:46:41 +0900

    :zqp: python 입출력 문법 복습

--- baekjoon/00_basic.py
+++ baekjoon/00_basic.py
@@ -1,43 +1,53 @@
 # print("HelloWorld")
-
+#
 # print("강한친구 대한육군\n강한친구 대한육군")
-
+#
 # print("\\    /\\\n )  ( \')\n(  /  )\n \\(__)|")
-
+#
 # print("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|")
-
-# A, B = map(int,input().split())
+#
+# A, B = map(int, input().split())
 # print(A+B)
-
+#
 # A, B = map(int,input().split())
 # print(A-B)
-
+#
 # A, B = map(int,input().split())
 # print(A*B)
-
+#
 # A, B = map(float,input().split())
 # print(A/B)
-
+#
 # A,B = map(int,input().split())
 # print(A+B)
 # print(A-B)
 # print(A*B)
 # print(A//B)
 # print(A%B)
-
+#
 # A, B, C = map(int,input().split())
 # print((A+B)%C)
 # print(((A%C)+(B%C))%C)
 # print((A*B)%C)
 # print(((A%C)*(B%C))%C)
-
+#
 # A=int(input())
 # B=int(input())
-
+#
 # print(A*(B%10))
 # print(A*(B//10%10))
 # print(A*(B//100))
 # print(A*B)
+
+##18108번
+# print(int(input()) - (2541-1998))
+
+##3003번
+# a,b,c,d,e,f = map(int, input().split())
+# print(1-a, 1-b, 2-c, 2-d, 2-e, 8-f)
+
+##25083
+# print("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |")
 
 
 #리스트 컴프리핸션 -> 2차원 리스트 초기화시 반드시 리스트 컴프리헨션 사용
