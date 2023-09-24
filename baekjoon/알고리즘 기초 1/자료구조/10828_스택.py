d7b458c38ee9c04401c6083fc68ee93be1c72a26
YoonbeomSo <ybum1224@naver.com>
2023-09-24 17:02:08 +0900

    :zap:10828_스택

--- baekjoon/알고리즘 기초 1/자료구조/10828_스택.py
+++ baekjoon/알고리즘 기초 1/자료구조/10828_스택.py
@@ -1,8 +1,10 @@
-N = int(input())
+import sys
+N = int(sys.stdin.readline())
+
 
 stack=[]
 for i in range(N):
-    message = input().split()
+    message = sys.stdin.readline().split()
     #push X
     if message[0] == 'push':
         stack.append(message[1])
@@ -27,3 +29,7 @@ for i in range(N):
             print(-1)
         else:
             print(stack[-1])
+
+
+#input() 함수를 사용할 경우, 시간초과 에러가 뜨므로 시간단축을 위해 sys.stdin.readline()을 사용한다.
+#입출력 속도 비교 : sys.stdin.readline > raw_input() > input()
